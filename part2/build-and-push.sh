5378459f8fce66c3d20335e83926c9208c8d3498
Fuminori Kikuchi <yakipote@redimpulz.com>
2020-06-27 09:53:46 +0900

    Delete build-and-push.sh

--- part2/build-and-push.sh
+++
@@ -1,5 +0,0 @@
-docker build -t redimpulz/nginx:part2 -f nginx/Dockerfile .
-docker build -t redimpulz/phpfpm:part2 -f phpfpm/Dockerfile .
-
-docker push redimpulz/nginx:part2
-docker push redimpulz/phpfpm:part2
